86c21648b6bcb0bd1fcf511eb057fea59c4c93d5
Luca <44023561+Casheh@users.noreply.github.com>
2021-05-06 20:40:30 -0400

    Added withdraw inventories

--- src/main/java/net/casheh/celllevel/inventory/WithdrawInventory.java
+++ src/main/java/net/casheh/celllevel/inventory/WithdrawInventory.java
@@ -1,12 +1,17 @@
 package net.casheh.celllevel.inventory;
 
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
 import fr.minuskube.inv.ClickableItem;
 import fr.minuskube.inv.SmartInventory;
 import fr.minuskube.inv.content.InventoryContents;
 import fr.minuskube.inv.content.InventoryProvider;
 import fr.minuskube.inv.content.SlotPos;
+import net.casheh.celllevel.CellLevel;
+import net.casheh.celllevel.managers.IslandUtilities;
+import net.casheh.celllevel.managers.PlayerUtilities;
 import net.casheh.celllevel.util.ItemBuilder;
 import net.casheh.celllevel.util.Util;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -72,8 +77,55 @@ public class WithdrawInventory implements InventoryProvider {
             lore = "&oClick here to withdraw &b" + amount + "&f beacons!";
         inventoryContents.set(1, 4, ClickableItem.of(new ItemBuilder(Material.BEACON).displayname("&b&lWITHDRAW").lore(" ").lore(lore).build(), e -> {
             int quantity = Util.getContainingNumber(Util.strip(e.getCurrentItem().getItemMeta().getLore().get(1)));
-            e.getWhoClicked().getInventory().addItem(new ItemStack(Material.BEACON, quantity));
+            withdraw((Player) e.getWhoClicked(), amount);
         }));
     }
+
+    private void withdraw(Player player, int amount) {
+        PlayerUtilities playerUtilities = new PlayerUtilities(player.getUniqueId());
+        IslandUtilities islandUtilities = new IslandUtilities(SuperiorSkyblockAPI.getPlayer(player).getIsland());
+        int emptySpace = Util.getEmptySpace(player.getInventory(), Material.BEACON);
+
+        if (playerUtilities.getBeacons() < amount || playerUtilities.getBeacons() == 0) {
+            player.closeInventory();
+            player.sendMessage(CellLevel.inst.getCfg().getPrefix() + Util.color("&cYou do not have enough beacons!"));
+            return;
+        }
+
+        if (emptySpace < amount) {
+            if (emptySpace == 0) {
+                player.closeInventory();
+                player.sendMessage(CellLevel.inst.getCfg().getPrefix() + Util.color("&cYour inventory is full!"));
+                return;
+            }
+
+            player.closeInventory();
+            player.sendMessage(CellLevel.inst.getCfg() + Util.color("&aYou have withdrawn &b" + emptySpace + "&a beacons!"));
+            player.getInventory().addItem(new ItemStack(Material.BEACON, emptySpace));
+
+            Bukkit.getScheduler().runTaskAsynchronously(CellLevel.inst, new Runnable() {
+                @Override
+                public void run() {
+                    playerUtilities.setBeacons(playerUtilities.getBeacons() - emptySpace);
+                    islandUtilities.setBeacons(islandUtilities.getBeacons() - emptySpace);
+                }
+            });
+            return;
+        }
+
+        player.closeInventory();
+        player.sendMessage(CellLevel.inst.getCfg() + Util.color("&aYou have withdrawn &b" + amount + "&a beacons!"));
+        player.getInventory().addItem(new ItemStack(Material.BEACON, amount));
+
+        Bukkit.getScheduler().runTaskAsynchronously(CellLevel.inst, new Runnable() {
+            @Override
+            public void run() {
+                playerUtilities.setBeacons(playerUtilities.getBeacons() - amount);
+                islandUtilities.setBeacons(islandUtilities.getBeacons() - amount);
+            }
+        });
+    }
+
+
 
 }
